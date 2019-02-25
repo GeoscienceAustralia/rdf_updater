2f7cdd4fabc8e4d1f56b8919504b9321694d4048
Alex Ip <alex@trentham.net.au>
2019-02-25 18:48:59 +1100

    __main__ module created Debugging output improved

--- rdf_updater/__init__.py
+++ rdf_updater/__init__.py
@@ -14,13 +14,13 @@ import re
 from pprint import pformat
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG) # Initial logging level for this module
+logger.setLevel(logging.INFO) # Initial logging level for this module
 logger.debug('__name__ = {}'.format(__name__))
 
 class RDFUpdater(object):
     settings = None
     
-    def __init__(self, settings_path=None, debug=True):
+    def __init__(self, settings_path=None, debug=False):
         
         # Initialise and set debug property
         self._debug = None
@@ -30,7 +30,7 @@ class RDFUpdater(object):
         settings_path = settings_path or os.path.join(package_dir, 'rdf_updater_settings.yml')
         self.settings = yaml.safe_load(open(settings_path))
         
-        logger.debug('Settings: {}'.format(pformat(self.settings)))
+        #logger.debug('Settings: {}'.format(pformat(self.settings)))
         
         
     def get_rdfs(self):
@@ -53,24 +53,25 @@ WHERE {?s ?p ?o .}'''
             else:
                 raise Exception('Bad source type for RDF')
             #logger.debug('http_method = {}, url = {}, headers = {}, params = {}, data = {}'.format(http_method, url, headers, params, data))
+            logger.info('Reading RDF from {} via {}'.format(url, rdf_config['source_type']))
             response = http_method(url, headers=headers, params=params, data=data, timeout=self.settings['timeout'])
             #logger.debug('Response content: {}'.format(str(response.content)))
             assert response.status_code == 200, 'Response status code != 200'
             return(response.content)
                 
-                
+        logger.info('Reading RDFs from sources to files')        
         for rdf_name, rdf_config in self.settings['rdf_configs'].items():
-            logger.debug('Processing {}'.format(rdf_name))
+            logger.info('Processing {}'.format(rdf_config['name']))
             try:
                 rdf = get_rdf(rdf_config)
                 #logger.debug('rdf = {}'.format(rdf))
-                logger.debug('Writing RDF to file {}'.format(rdf_config['rdf_file_path']))
+                logger.info('Writing RDF to file {}'.format(rdf_config['rdf_file_path']))
                 with open(rdf_config['rdf_file_path'], 'wb') as rdf_file:
                     rdf_file.write(rdf)
             except Exception as e:
                 logger.warning('RDF get from {} to file failed: {}'.format(rdf_config['source_type'], e))
                 
-        logger.debug('Finished')
+        logger.info('Finished reading to files')
         
         
     def put_rdfs(self):
@@ -79,26 +80,27 @@ WHERE {?s ?p ?o .}'''
             headers = {'Content-Type': 'application/rdf+xml'}
             params = {'graph': rdf_config['uri']}
             
-            logger.debug('Writing RDF to {}'.format(url))
+            logger.info('Writing RDF to {}'.format(url))
             response = requests.put(url, headers=headers, params=params, data=rdf, timeout=self.settings['timeout'])
             #logger.debug('Response content: {}'.format(response.content))
             assert response.status_code == 200 or response.status_code == 201, 'Response status code {}  != 200 or 201: {}'.format(response.status_code, response.content)
             return(response.content)
                 
-                
+        logger.info('Writing RDFs to triple-store {} from files'.format(self.settings['triple_store_url']))           
         for rdf_name, rdf_config in self.settings['rdf_configs'].items():
-            logger.debug('Processing {}'.format(rdf_name))
+            logger.info('Processing {}'.format(rdf_config['name']))
             try:
-                logger.debug('Reading RDF from {}'.format(rdf_config['rdf_file_path']))
+                logger.info('Reading RDF from {}'.format(rdf_config['rdf_file_path']))
                 with open(rdf_config['rdf_file_path'], 'rb') as rdf_file:
                     rdf = rdf_file.read()
                 #logger.debug('rdf = {}'.format(rdf))
                 result = json.loads(put_rdf(rdf_config, rdf))
-                logger.debug('result = {}'.format(result))
+                #logger.debug('result = {}'.format(result))
+                logger.info('{} triples (re)written'.format(result['tripleCount']))
             except Exception as e:
                 logger.warning('RDF put from file to triple-store failed: {}'.format(e))
                 
-        logger.debug('Finished')
+        logger.info('Finished writing to triple-store')
         
         
     @property
@@ -117,26 +119,3 @@ WHERE {?s ?p ?o .}'''
                 
         logger.debug('Logger {} set to level {}'.format(logger.name, logger.level))
 
-
-def main():
-    rdf_updater = RDFUpdater() 
-    #logger.debug(pformat(rdf_updater.__dict__))
-    rdf_updater.get_rdfs()
-    
-    rdf_updater.put_rdfs()
-    
-    
-    
-    
-if __name__ == '__main__':
-    # Setup logging handlers if required
-    if not logger.handlers:
-        # Set handler for root root_logger to standard output
-        console_handler = logging.StreamHandler(sys.stdout)
-        #console_handler.setLevel(logging.INFO)
-        console_handler.setLevel(logging.DEBUG)
-        console_formatter = logging.Formatter('%(message)s')
-        console_handler.setFormatter(console_formatter)
-        logger.addHandler(console_handler)
-
-    main()
---
+++ rdf_updater/__main__.py
@@ -0,0 +1,41 @@
+'''
+Main routine for Class RDFUpdater
+Created on 25 Feb. 2019
+
+@author: Alex Ip
+'''
+import logging
+import sys
+from rdf_updater import RDFUpdater
+
+logger = logging.getLogger('rdf_updater')
+
+DEBUG = False
+
+def main():
+    rdf_updater = RDFUpdater(debug=DEBUG) 
+    #logger.debug(pformat(rdf_updater.__dict__))
+    
+    print()
+    
+    rdf_updater.get_rdfs() # Read RDFs from sources into files
+    
+    print()
+    
+    rdf_updater.put_rdfs() # Write RDFs to triple-store from files
+    
+    
+    
+    
+if __name__ == '__main__':
+    # Setup logging handler if required
+    if not logger.handlers:
+        # Set handler for root root_logger to standard output
+        console_handler = logging.StreamHandler(sys.stdout)
+        #console_handler.setLevel(logging.INFO)
+        console_handler.setLevel(logging.DEBUG)
+        console_formatter = logging.Formatter('%(message)s')
+        console_handler.setFormatter(console_formatter)
+        logger.addHandler(console_handler)
+
+    main()
