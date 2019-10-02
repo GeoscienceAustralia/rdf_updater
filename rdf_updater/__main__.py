690207f7bdb1361759ab1462a1540ce7f20ce83a
Alex Ip <alex.ip@ga.gov.au>
2019-10-02 11:32:26 +1000

    Command line arguments updated with '--update_config'

--- rdf_updater/__main__.py
+++ rdf_updater/__main__.py
@@ -20,10 +20,7 @@ def main():
     parser.add_argument("-s", "--settings", help="Settings file", type=str)
     parser.add_argument('-n', '--no_skosify', action='store_const', const=True, default=False,
                         help='Do not perform SKOS validation and inferencing. Default is SKOS enabled')
-    parser.add_argument('-d', '--update_directories', action='store_const', const=True, default=False,
-                        help='Update vocab settings from directories. Default is no update')
-    parser.add_argument('-g', '--update_github', action='store_const', const=True, default=False,
-                        help='Update vocab settings from GitHub. Default is no update')
+    parser.add_argument('-u', '--update_config', help='Update RDF config from "directories", "github" or "csiro"', type=str)
     parser.add_argument('--debug', action='store_const', const=True, default=False,
                         help='output debug information. Default is no debug info')
     
@@ -32,25 +29,26 @@ def main():
     skosify = not args.no_skosify
 
     rdf_updater = RDFUpdater(settings_path=args.settings, 
-                             update_github=args.update_github, 
-                             update_directories=args.update_directories,
+                             update_config=[update_source.strip() for update_source in (args.update_config or '').split(',')], 
                              debug=args.debug
                              ) 
 
     rdf_updater.get_rdfs() # Read RDFs from sources into files
-
-    print()
  
+    print()
+  
     if skosify:
         rdf_updater.skosify_rdfs()
         print()
-
+ 
     rdf_updater.write_rdfs_to_triple_stores(skosified=skosify) # Write RDFs to triple-stores from files
-
+  
     print()
+  
+    #rdf_updater.resolve_ConceptScheme_indirection() # Test indirecton fix
 
-    rdf_updater.output_summary_text() # Output text summary to file
-
+    #rdf_updater.output_summary_text() # Output text summary to file
+    
 
 if __name__ == '__main__':
     # Setup logging handler if required
